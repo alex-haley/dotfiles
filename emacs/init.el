d750362e6b08a0dd1d4ed966d0b76da43dea56a9
alex-haley <mloyygraduate7@gmail.com>
2025-02-26 07:31:32 +0800

    updates emacs conf

--- emacs/init.el
+++ emacs/init.el
@@ -66,6 +66,7 @@
 (global-unset-key (kbd "C-M-e"))
 (global-unset-key (kbd "C-M-r"))
 (global-unset-key (kbd "M-S-i"))
+(global-unset-key (kbd "C-v"))
 
 ;; SETTING
 
@@ -87,8 +88,21 @@
 (keymap-global-set "C-e"     'recenter-top-bottom)
 (keymap-global-set "C-l"     'kill-line)
 (keymap-global-set "M-i"     'list-buffers)
+(keymap-global-set "C-v"     'yank)
+(keymap-global-set "C-c C-c" 'copy-region-as-kill)
+(keymap-global-set "C-w"     'kill-region)
 
 ;; KEYBINDINGS END
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+;; and `package-pinned-packages`. Most users will not need or want to do this.
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+
+;; highlight-numbers
+(add-hook 'prog-mode-hook 'highlight-numbers-mode)
 
 ;; Dired
 (require 'dired)
@@ -105,7 +119,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(magit evil)))
+ '(package-selected-packages
+   '(emacs-gc-stats zig-mode parent-mode highlight-numbers ## magit evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
